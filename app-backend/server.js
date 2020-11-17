05c59c745228f797fc31b91696dcdb23022f9097
alexander_aquino <nanito169@gmail.com>
2020-11-17 18:56:40 -0500

    created controller folder

--- app-backend/server.js
+++ app-backend/server.js
@@ -15,6 +15,8 @@ mongoose.connect('mongodb://localhost:27017/store_db', { useNewUrlParser:true })
 mongoose.connection.once('open', () => {
     console.log('connected to db')
 })
+mongoose.connection.on('error', err =>console.log(err.message))
+mongoose.connection.on('disconnected', () =>console.log('mongo disconnected'))
 
 app.listen(port, () => {
     console.log('app is running on port:',port)
