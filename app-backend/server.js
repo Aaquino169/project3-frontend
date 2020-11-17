1eeb2446492865773e44c48721c650e1083a6f73
alexander_aquino <nanito169@gmail.com>
2020-11-17 18:24:34 -0500

    created file structure,established connection with express and mongoose

---
+++ app-backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const mongoose = require('mongoose')
+
+const port = 8000
+
+app.use(express.json())
+
+
+
+//middleware for mongoose
+mongoose.connect('mongodb://localhost:27017/store_db', { useNewUrlParser:true })
+mongoose.connection.once('open', () => {
+    console.log('connected to db')
+})
+
+app.listen(port, () => {
+    console.log('app is running on port:',port)
+})
