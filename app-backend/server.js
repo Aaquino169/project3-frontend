ff8cc3e9c4b5eadcde702f5c8aee9b4b84eeaa11
Edward Dorta <dortaedward@gmail.com>
2020-11-19 13:34:17 -0500

    Started Front end

--- app-backend/server.js
+++ app-backend/server.js
@@ -4,7 +4,22 @@ const app = express()
 
 const mongoose = require('mongoose')
 
-const port = 8000 
+const cors = require('cors')
+
+const port = 8000
+
+// const whitelist = ['http://localhost:3000']
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error('Not allowed by CORS'))
+//       }
+//     }
+// }
+//
+// app.use(cors(corsOptions))
 
 app.use(express.json())
 
@@ -15,6 +30,12 @@ mongoose.connect('mongodb://localhost:27017/store_db', { useNewUrlParser:true })
 mongoose.connection.once('open', () => {
     console.log('connected to db')
 })
+mongoose.connection.on('error', err =>console.log(err.message))
+mongoose.connection.on('disconnected', () =>console.log('mongo disconnected'))
+
+const storeController = require('./controllers/store')
+app.use('/', storeController)
+
 
 app.listen(port, () => {
     console.log('app is running on port:',port)
---
+++ app-frontend/src/Components/NavComponent/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+
+export default function NavBar(props){
+    return(
+        <div class='header-container'>
+        
+            <div class ='logo'>
+                <h1>
+                    logo
+                </h1>
+            </div>
+
+            <div class ='search-bar'>
+                <select id='search' name='search'>
+
+                    <option value='all'> All</option>
+                    <option value='keyboards'> Keyboards</option>
+                    <option value='keycaps'> Keycaps</option>
+                    <option value='cases'> Cases</option>
+                    <option value='pcb-boards'> Pcb Boards</option>
+                    <option value='switches'> Switches</option>
+                    <option value='accessories'> Accessories</option>
+
+                </select>
+                <input type='text' id='searchbar' name='searchbar' placeholder='Search...' />
+                <input type='submit' class='search-btn' />
+            </div>
+
+            <nav>
+
+                <ul>
+                    <td>
+                        Log In/Sign Up
+                    </td>
+                    <td>
+                        Orders & Returns
+                    </td>
+                    <td>
+                        Cart
+                    </td>
+                </ul>
+
+            </nav>
+        </div>
+    )
+}
