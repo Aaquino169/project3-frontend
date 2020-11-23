d2564b86077cf08c5f5f538bb29994588bf16fe5
alexander_aquino <nanito169@gmail.com>
2020-11-22 22:10:33 -0500

    added seed route

--- app-backend/controllers/users.js
+++ app-backend/controllers/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const user = express.Router()
 const Users = require('../models/users')
+const Merch = require('../models/merch')
 
 //create route
 user.post('/new', async (req,res) => {
@@ -14,5 +15,20 @@ user.post('/new', async (req,res) => {
         res.status(200).send(createdUser)
     })
 })
+
+// user.put('/addToCart/:id', async (req/res) => {
+//     try {
+//         const targetMerch = await Merch.findById(req.params.id)
+//         Users.cart.push(targetMerch)
+
+//     } catch (error) {
+        
+//     }
+// })
+
+// user.put('/addToCart/:id', async (req/res) => {
+    
+// })
+
 
 module.exports = user
